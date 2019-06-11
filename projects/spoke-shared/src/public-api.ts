49b589ccf71bfe33fecc97e8caef536c51478dae
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2019-06-10 23:26:32 -0400

    get rid of lib for now ... will add it back when we're ready for that

--- projects/spoke-shared/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of spoke-shared
- */
-
-export * from './lib/spoke-shared.service';
-export * from './lib/spoke-shared.component';
-export * from './lib/spoke-shared.module';
